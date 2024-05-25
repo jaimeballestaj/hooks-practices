85bde01874513cf2daf9b10e1a5eda33c9753667
inesmariao <inesmao.misena@gmail.com>
2024-05-24 19:36:35 -0500

    useState SecondComponent.jsx

---
+++ src/components/SecondComponent.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export const SecondComponent = () => {
+
+  const [ name, setName ] = useState("Inés María");
+  const [ isNameChanged, setIsNameChanged ] = useState(false);
+
+  const changeName = (e) => {
+    if (isNameChanged) {
+      setName("Inés María");
+    } else {
+      setName("Pedro Oliveros");
+    }
+    setIsNameChanged(!isNameChanged);
+  }
+
+  return (
+    <div>
+      <h3>Componente: SecondComponent</h3>
+      <p>{ name }</p>
+      <div>
+        <button className='button-large' onClick={ changeName }>Cambiar nombre</button>
+      </div>
+    </div>
+  )
+}
